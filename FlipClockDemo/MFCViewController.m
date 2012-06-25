4ecef64299de65e46276afc38c844835ab6b9b72
Christopher Bowns <github@cbowns.com>
2012-06-25 13:18:07 -0700

    Snapshot two: - View animations run back-to-back (explicitly setting beginTime and using duration as an offset works) - Layers now transform their own camera and don't use a sublayer transform of their parent view - Added a helper function for doing anchorPoint/center offset calculations.

--- FlipClockDemo/MFCViewController.m
+++ FlipClockDemo/MFCViewController.m
@@ -77,6 +77,11 @@
 - (void)viewWasTapped:(UITapGestureRecognizer *)tapGestureRecognizer;
 {
 	NSLog(@"%s %@", __func__, tapGestureRecognizer);
+
+	///////////////////////
+	// View snapshotting //
+	///////////////////////
+
 	UIView *aView = tapGestureRecognizer.view;
 
 	// Render the tapped view into an image:
@@ -112,11 +117,79 @@
 	UIGraphicsEndImageContext();
 
 	UIImageView *topHalfView = [[UIImageView alloc] initWithImage:top];
+	topHalfView.frame = CGRectOffset(topHalfView.frame, 20.f, 0.f); // move in 20 px
 	[self.view addSubview:topHalfView];
 
 	UIImageView *bottomHalfView = [[UIImageView alloc] initWithImage:bottom];
-	bottomHalfView.frame = CGRectOffset(bottomHalfView.frame, 0.f, topHalfView.frame.size.height);
+	bottomHalfView.frame = topHalfView.frame;
+	bottomHalfView.frame = CGRectOffset(bottomHalfView.frame, 20.f, topHalfView.frame.size.height * 3); // temp move in 20 px
 	[self.view addSubview:bottomHalfView];
+
+
+
+	////////////////
+	// Animations //
+	////////////////
+
+	// Skewed identity for camera perspective:
+	CATransform3D skewedIdentityTransform = CATransform3DIdentity;
+	float zDistance = 1000;
+	skewedIdentityTransform.m34 = 1.0 / -zDistance;
+	// We use this instead of setting a sublayer transform on our view's layer,
+	// because that gives an undesirable skew on views not centered horizontally.
+
+	// Top tile:
+	// Set the anchor point to the bottom edge:
+	CGPoint newTopViewAnchorPoint = CGPointMake(0.5, 1.0);
+	CGPoint newTopViewCenter = [self center:topHalfView.center movedFromAnchorPoint:topHalfView.layer.anchorPoint toAnchorPoint:newTopViewAnchorPoint withFrame:topHalfView.frame];
+	topHalfView.layer.anchorPoint = newTopViewAnchorPoint;
+	topHalfView.center = newTopViewCenter;
+
+	// Add an animation to swing from top to bottom.
+	CABasicAnimation *topAnim = [CABasicAnimation animationWithKeyPath:@"transform"];
+	topAnim.beginTime = CACurrentMediaTime();
+	topAnim.duration = 2.5f;
+	topAnim.fromValue = [NSValue valueWithCATransform3D:skewedIdentityTransform];
+	topAnim.toValue = [NSValue valueWithCATransform3D:CATransform3DRotate(skewedIdentityTransform, -M_PI, 1.f, 0.f, 0.f)];
+	topAnim.delegate = self;
+	topAnim.removedOnCompletion = NO;
+	topAnim.fillMode = kCAFillModeForwards;
+	[topHalfView.layer addAnimation:topAnim forKey:@"topDownFlip"];
+
+	// Bottom tile:
+	// Change its anchor point:
+	CGPoint newAnchorPointBottomHalf = CGPointMake(0.5f, 0.f);
+	CGPoint newBottomHalfCenter = [self center:bottomHalfView.center movedFromAnchorPoint:bottomHalfView.layer.anchorPoint toAnchorPoint:newAnchorPointBottomHalf withFrame:bottomHalfView.frame];
+	bottomHalfView.layer.anchorPoint = newAnchorPointBottomHalf;
+	bottomHalfView.center = newBottomHalfCenter;
+
+	// Add an animation to swing from top to bottom.
+	CABasicAnimation *bottomAnim = [CABasicAnimation animationWithKeyPath:@"transform"];
+	bottomAnim.beginTime = topAnim.beginTime + topAnim.duration;
+	bottomAnim.duration = topAnim.duration;
+	bottomAnim.fromValue = [NSValue valueWithCATransform3D:CATransform3DRotate(skewedIdentityTransform, M_PI, 1.f, 0.f, 0.f)];
+	bottomAnim.toValue = [NSValue valueWithCATransform3D:skewedIdentityTransform];
+	bottomAnim.delegate = self;
+	bottomAnim.removedOnCompletion = NO;
+	bottomAnim.fillMode = kCAFillModeBoth;
+	[bottomHalfView.layer addAnimation:bottomAnim forKey:@"topDownFlip"];
+
+//	anim2.beginTime = anim1.beginTime + anim1.duration;
+//	anim3.beginTime = anim2.beginTime + anim2.duration;
+
+}
+
+// Scales center points by the difference in their anchor points scaled to their frame size.
+// Lets you move anchor points around without dealing with CA's implicit frame math.
+- (CGPoint)center:(CGPoint)oldCenter movedFromAnchorPoint:(CGPoint)oldAnchorPoint toAnchorPoint:(CGPoint)newAnchorPoint withFrame:(CGRect)frame;
+{
+	NSLog(@"%s moving center (%.2f, %.2f) from oldAnchor (%.2f, %.2f) to newAnchor (%.2f, %.2f)", __func__,
+		  oldCenter.x, oldCenter.y, oldAnchorPoint.x, oldAnchorPoint.y, newAnchorPoint.x, newAnchorPoint.y);
+	CGPoint anchorPointDiff = CGPointMake(newAnchorPoint.x - oldAnchorPoint.x, newAnchorPoint.y - oldAnchorPoint.y);
+	CGPoint newCenter = CGPointMake(oldCenter.x + (anchorPointDiff.x * frame.size.width), 
+									oldCenter.y + (anchorPointDiff.y * frame.size.height));
+	NSLog(@"%s new center is (%.2f, %.2f) (frame size: (%.2f, %.2f))", __func__, newCenter.x, newCenter.y, frame.size.width, frame.size.height);
+	return newCenter;
 }
 
 @end
